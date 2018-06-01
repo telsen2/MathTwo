4ad2e80c5e2491af4c382ec438108d23ad991cac
GBTC441002UR <GBTC441002UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-06-01 10:33:18 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int mynumber;
+        double myothernumber;
+
+        mynumber = 2;
+        myothernumber = 1.7938;
+
+        System.out.println("My number is  " + mynumber + " and " + " my other number is " + myothernumber);
+	// write your code here
+    }
+}
